a1e9fc7788564d9d0ed18848e9b217af3a133b8a
MD. NIZAM UDDIN TUHIN <112401451+md-nizam-uddin-tuhin-bhl@users.noreply.github.com>
2024-02-25 07:49:59 +0600

    Create Customer All Services

---
+++ API/AuthGuad/AuthGuad/Helper/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthGuad.Helper
+{
+    public class ApiResponse
+    {
+        public string Response { get; set; }
+        public string Result { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
--- API/AuthGuad/AuthGuad/Services/IcustomerService.cs
+++ API/AuthGuad/AuthGuad/Services/IcustomerService.cs
@@ -1,10 +1,15 @@
 ﻿using AuthGuad.Dto;
+using AuthGuad.Helper;
 using AuthGuad.Models;
 
 namespace AuthGuad.Services
 {
     public interface IcustomerService
     {
-        Task<List<CustomerDto>> Customer();
+        Task<List<CustomerDto>> GetCustomer();
+        Task<CustomerDto> GetByCode(string code);
+        Task<ApiResponse> Remove(string code);
+        Task<ApiResponse> Create(CustomerDto data);
+        Task<ApiResponse> Update(CustomerDto data,string code);
     }
 }
