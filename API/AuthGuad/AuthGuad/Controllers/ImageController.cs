a836b5b360ab055f039668749ebd3e071c0da699
MD. NIZAM UDDIN TUHIN <112401451+md-nizam-uddin-tuhin-bhl@users.noreply.github.com>
2024-02-27 14:57:04 +0600

    Add Multiple Image Upload and Get

--- API/AuthGuad/AuthGuad/Controllers/ImageController.cs
+++ API/AuthGuad/AuthGuad/Controllers/ImageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AuthGuad.Helper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
@@ -16,24 +17,59 @@ namespace AuthGuad.Controllers
         }
 
         [HttpPost("UploadImage")]
-        public async Task<IActionResult> UploadImage()
+        public async Task<IActionResult> UploadImage(IFormFile formFile, string icode)
         {
-            bool result = false;
-          
+            ApiResponse response = new ApiResponse();
+
             try
             {
-                var _uploadedfile = Request.Form.Files;
-                foreach (IFormFile source in _uploadedfile)
+                string FilePath = GetFilePath(icode);
+
+                if (!System.IO.Directory.Exists(FilePath))
                 {
-                    string UserName = source.FileName;
-                    string FilePath = GetFilePath(UserName);
+                    System.IO.Directory.CreateDirectory(FilePath);
+                }
 
-                    if (!System.IO.Directory.Exists(FilePath)) 
-                    {
-                        System.IO.Directory.CreateDirectory(FilePath);  
-                    }
+                string imagePath = FilePath + "\\" + icode + ".png";
+                if (System.IO.Directory.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
 
-                    string imagePath = FilePath + "\\image.png";
+                }
+                using (FileStream stream = System.IO.File.Create(imagePath))
+                {
+                    await formFile.CopyToAsync(stream);
+                    response.ResponseCode = 200;
+                    response.Result = "pass";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = ex.Message;
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("MultiUploadImage")]
+        public async Task<IActionResult> MultiUploadImage(IFormFileCollection formcollection, string icode)
+        {
+           ApiResponse response = new ApiResponse();
+            int passcount = 0; int errorcount =0;
+            try
+            {
+                string FilePath = GetFilePath(icode);
+
+                if (!System.IO.Directory.Exists(FilePath))
+                {
+                    System.IO.Directory.CreateDirectory(FilePath);
+                }
+                foreach (var file in formcollection)
+                {
+
+                    string imagePath = FilePath + "\\" + file.FileName;
+
                     if (System.IO.Directory.Exists(imagePath))
                     {
                         System.IO.File.Delete(imagePath);
@@ -41,22 +77,94 @@ namespace AuthGuad.Controllers
                     }
                     using (FileStream stream = System.IO.File.Create(imagePath))
                     {
-                        await source.CopyToAsync(stream);
-                        result = true;
+                        await file.CopyToAsync(stream);
+                        passcount++;
+                       
                     }
                 }
+               
 
             }
             catch(Exception ex)
             {
-               
+               errorcount++;
+               response.ErrorMessage = ex.Message;
             }
-            return Ok(result);
+            response.ResponseCode = 200;
+            response.Result = passcount + "File Uploaded & " + errorcount + " file file";
+            return Ok(response);
+        }
+
+
+        [HttpGet("GetImage")]
+        public async Task<IActionResult> GetImage(string icode)
+        {
+            string ImageUrl = string.Empty;
+            string hostUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            try
+            {
+                string FilePath = GetFilePath(icode);
+                string imagePath = FilePath + "\\" + icode + ".png";
+                if(System.IO.File.Exists(imagePath))
+                {
+                    ImageUrl = hostUrl + "/Upload/Image/"+icode+"/"+icode+".png";
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Ok(ImageUrl);
+        } 
+        
+        [HttpGet("GetMultiImage")]
+        public async Task<IActionResult> GetMultiImage(string icode)
+        {
+            List<string> ImageUrl = new List<string>();
+            string hostUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            try
+            {
+                string FilePath = GetFilePath(icode);
+                if (System.IO.Directory.Exists(FilePath))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(FilePath);
+                    FileInfo[] fileInfos = directoryInfo.GetFiles();
+                    foreach (FileInfo fileInfo in fileInfos)
+                    {
+                       string filename = fileInfo.Name;
+                       string imagePath = FilePath + "\\" + filename;
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                           string _ImageUrl = hostUrl + "/Upload/Image/" + icode + "/" + filename;
+                            ImageUrl.Add(_ImageUrl);
+                        }
+                    }
+                  
+
+                }
+
+               
+                
+                
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Ok(ImageUrl);
         }
         [NonAction]
-       private string GetFilePath(string UserName)
+       private string GetFilePath(string icode)
         {
-            return this.environment.WebRootPath + "\\images" + UserName;
+            return this.environment.WebRootPath + "\\Upload\\Image\\" + icode;
         }
     }
 }
--- API/AuthGuad/AuthGuad/Program.cs
+++ API/AuthGuad/AuthGuad/Program.cs
@@ -62,7 +62,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
