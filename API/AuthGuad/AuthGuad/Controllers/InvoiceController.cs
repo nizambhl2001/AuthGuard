fb7427995c658008844ed42f9f4cf88af9423726
MD. NIZAM UDDIN TUHIN <112401451+md-nizam-uddin-tuhin-bhl@users.noreply.github.com>
2024-03-07 12:30:41 +0600

    ADD PDFwithImag

--- API/AuthGuad/AuthGuad/Controllers/InvoiceController.cs
+++ API/AuthGuad/AuthGuad/Controllers/InvoiceController.cs
@@ -1,7 +1,10 @@
 ﻿using AuthGuad.Dto;
 using AuthGuad.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PdfSharpCore;
+using PdfSharpCore.Pdf;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+
 
 namespace AuthGuad.Controllers
 {
@@ -10,10 +13,12 @@ namespace AuthGuad.Controllers
     public class InvoiceController : ControllerBase
     {
         private readonly IInvoiceServices _invoice;
+        private readonly IWebHostEnvironment environment;
 
-        public InvoiceController(IInvoiceServices invoice)
+        public InvoiceController(IInvoiceServices invoice, IWebHostEnvironment environment)
         {
             _invoice = invoice;
+            this.environment = environment;
         }
         [HttpGet("GetAllInoviceHeaders")]
         public async Task<List<InoviceHeader>> GetAllInoviceHeaders()
@@ -41,6 +46,38 @@ namespace AuthGuad.Controllers
         {
             return await _invoice.GetInoviceDetailsByCodeAsync(invoiceNo);
 
+        }
+        [HttpGet("GenPDFwithImage")]
+        public async Task<IActionResult> GenPDFwithImage()
+        {
+            var document = new PdfDocument();
+            string htmlelement = "<div style='width:100%'>";
+            // string imgeurl = "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg";
+            //string imgeurl = "https://" + HttpContext.Request.Host.Value + "/Uploads/common/logo.jpeg";
+            string imgeurl = "data:image/png;base64, " + Getbase64string() + "";
+            htmlelement += "<img style='width:80px;height:80%' src='" + imgeurl + "'   />";
+            htmlelement += "<h2>Welcome to <br> Eng. MD NIZAM UDDIN TUHIN</h2>";
+            htmlelement += "<h2>Software Arena Limited</h2>";
+            htmlelement += "</div>";
+            PdfGenerator.AddPdfPages(document, htmlelement, PageSize.A4);
+    
+            byte[] response = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                document.Save(ms);
+                response = ms.ToArray();
+            }
+            return File(response, "application/pdf", "PDFwithImage.pdf");
+
+        }
+
+        [NonAction]
+        public string Getbase64string()
+        {
+            string filepath = this.environment.WebRootPath + "\\Upload\\common\\logo.jpg";
+            byte[] imgarray = System.IO.File.ReadAllBytes(filepath);
+            string base64 = Convert.ToBase64String(imgarray);
+            return base64;
         }
     }
 }
